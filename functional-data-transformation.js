88120cfb17887d1bd4d210c0c1d897350023b245
kewablay <kojokewajunior123@gmail.com>
2024-06-11 16:15:59 +0000

    feat: added doubleArray function.

--- functional-data-transformation.js
+++ functional-data-transformation.js
@@ -1,35 +1,49 @@
 // 1.
 // a. capitalize strings
 const capitalize = (str) => {
-    return str.toUpperCase();
-  };
-  
-  console.log(capitalize("Hello kewa blay"))
+  return str.toUpperCase();
+};
 
+console.log(capitalize("Hello kewa blay"));
 
 // b. Reverse a string
 const reverse = (str) => {
-    return str.split("").reverse().join("")
-}
+  return str.split("").reverse().join("");
+};
 
-console.log(reverse("hello kewa blay"))
+console.log(reverse("hello kewa blay"));
 
 // c. Check if a string is a palindrome
 const isPalindrome = (str) => {
-    let isPalindrome =  str === str.split("").reverse().join("")
-    if(isPalindrome) {
-        return "The input string is a palindrome. "
-    } else {
-        return "The input string is not a palindrome."
-    }
-}
+  let isPalindrome = str === str.split("").reverse().join("");
+  if (isPalindrome) {
+    return "The input string is a palindrome. ";
+  } else {
+    return "The input string is not a palindrome.";
+  }
+};
 
-console.log(isPalindrome("madam"))
+console.log(isPalindrome("madam"));
 
-
-// d. Word count - Counts the number of words in a string 
+// d. Word count - Counts the number of words in a string
 
 const wordCount = (str) => {
-    let count =  str.split(" ").length()
-    return "The number of words in the stirng is: ", count
-}
+  let count = str.split(" ").length();
+  return "The number of words in the stirng is: ", count;
+};
+
+// 2.
+// a. Double array - doubles every number in an array
+
+const doubleArray = (arr) => {
+  return arr.map((element) => {
+    // if element is a not a number return the element
+    if (isNaN(parseInt(element))) return element;
+
+    // else double the number
+    return parseInt(element) * 2;
+  });
+};
+
+console.log(doubleArray(["a", 1, "b", 2, "c", 3]));
+

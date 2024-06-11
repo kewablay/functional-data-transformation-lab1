b65c8c40f51bddaf3fd83eb7d093410774cd2bba
kewablay <kojokewajunior123@gmail.com>
2024-06-11 15:52:10 +0000

    feat: added isPalindrome function

--- functional-data-transformation.js
+++ functional-data-transformation.js
@@ -14,6 +14,16 @@ const reverse = (str) => {
 
 console.log(reverse("hello kewa blay"))
 
+// c. Check if a string is a palindrome
+const isPalindrome = (str) => {
+    let isPalindrome =  str === str.split("").reverse().join("")
+    if(isPalindrome) {
+        return "The input string is a palindrome. "
+    } else {
+        return "The input string is not a palindrome."
+    }
+}
 
+console.log(isPalindrome("madam"))
 
 
